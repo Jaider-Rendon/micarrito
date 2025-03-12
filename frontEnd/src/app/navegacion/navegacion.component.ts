1a818aa402de960fc0fc4e36c3092266f6b7fcab
Jaider-Rendon <jaiderandresrendon@gmail.com>
2025-03-12 00:51:58 -0500

    botones

--- frontEnd/src/app/navegacion/navegacion.component.ts
+++ frontEnd/src/app/navegacion/navegacion.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "../login/login.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navegacion',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent,],
   templateUrl: './navegacion.component.html',
   styleUrl: './navegacion.component.css'
 })
 export class NavegacionComponent {
 
+constructor(private router:Router){
+
+}  
+
+
+  abrirsesion(){
+    
+    this.router.navigate(['./login'])
+
+  }
+  Registro(){
+    this.router.navigate(['./register'])
+  }
+
 }
+
