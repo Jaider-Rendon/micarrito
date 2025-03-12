d44dfe7754da6ca94e6f9327f6fb90f162f4d8d7
Jaider-Rendon <jaiderandresrendon@gmail.com>
2025-03-12 16:25:10 -0500

    listo

--- frontEnd/src/app/navegacion/navegacion.component.ts
+++ frontEnd/src/app/navegacion/navegacion.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
@@ -5,19 +6,20 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-navegacion',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './navegacion.component.html',
   styleUrl: './navegacion.component.css'
 })
 export class NavegacionComponent {
+ver: boolean
 
 constructor(private router:Router){
-
+ this.ver = true;
 }  
 
 
   abrirsesion(){
-    
+    this.ver = false;
     this.router.navigate(['./login'])
 
   }
